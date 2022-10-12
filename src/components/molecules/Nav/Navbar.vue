434490fd7801954122841f9454eeeb59f5f08c32
Bianca Corrêa <bee.correa@hotmail.com>
2022-10-12 18:40:15 -0300

    feat: site description

--- src/components/molecules/Nav/Navbar.vue
+++ src/components/molecules/Nav/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
-  <section class="min-w-fit max-w-lg flex flex-row justify-between">
-    <h1 class="inline-block text-2xl justify-self-start">#bannerify</h1>
+  <section class="max-w-lg flex flex-row justify-between">
+    <article>
+      <h1 class="text-2xl">#bannerify</h1>
+      <p class="text-sm font-semibold py-2 text-gray-500 break-words">
+        Banners for your functions/class/whatever
+      </p>
+    </article>
     <article class="space-x-8">
       <ExternalLink :href="github" icon="devicon-github-original colored" size="text-2xl" />
       <ExternalLink :href="twitter" icon="devicon-twitter-original" size="text-2xl" />
