83803aa24f52d4fbdae437225a8f5779d8313241
Bianca Corrêa <bee.correa@hotmail.com>
2022-10-12 18:07:55 -0300

    refac: always pads from left to right

--- src/lib/banner/border.ts
+++ src/lib/banner/border.ts
@@ -4,13 +4,11 @@ export type BorderConfig = {
   padding: number;
   repeatable: string;
 }
-
 export const buildBorder = (title: string, { padding, repeatable }: BorderConfig) => repeatable + withBorders(title, { padding, repeatable }) + repeatable + "\n"
-export const buildTitle = (title: string, { padding, repeatable }: BorderConfig) => repeatable + withTitle(title, { padding, repeatable }) + repeatable + "\n"
+export const buildTitle = (title: string, { padding, repeatable }: BorderConfig) => repeatable + withTitle(title, { padding, repeatable: " " }) + repeatable + "\n"
 
-export const withBorders =
-  (title: string, { padding, repeatable }: BorderConfig) =>
-    pad(repeatable, padding) + pad(repeatable, title.length) + pad(repeatable, padding)
+export const withBorders = (title: string, { padding, repeatable }: BorderConfig) =>
+  pad(repeatable, padding) + pad(repeatable, title.length) + pad(repeatable, padding)
 
 export const withTitle = (title: string, { padding, repeatable }: BorderConfig) =>
   pad(repeatable, padding) + title + pad(repeatable, padding)
