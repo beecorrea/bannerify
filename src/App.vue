21f0d88c36e9db2e3aa631da6de2f6fc23e7c426
Bianca Corrêa <bee.correa@hotmail.com>
2022-10-26 17:32:04 -0300

    feat: removes border list from main page (#3)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
-import Border from './components/molecules/Banner/Border.vue'
+
 import Title from './components/molecules/Banner/Title.vue'
 import Banner from './components/molecules/Banner/Banner.vue'
 import Navbar from './components/molecules/Nav/Navbar.vue'
 
-import { borders } from "@stores/banner/border"
 
 </script>
 <template>
-  <main class="w-3/6 h-screen container mx-auto my-12 py-20 px-24 font-mono">
+  <main class="lg:w-3/6 w-full h-screen container mx-auto my-12 py-20 px-2 sm:px-12 lg:px-24 font-mono">
     <div class="max-w-md mx-auto">
       <Navbar />
-      <section id="controls" class="mt-14 min-w-fit max-w-xl grid grid-cols-2 grid-rows-2 gap-x-8 gap-y-12 ">
-        <Border class="md:col-span-1 col-span-2 max-w-fit" v-for="{name, value} in borders" :title="value" :part="name"
-          :key="name" />
-      </section>
-      <article class="flex flex-col flex-wrap justify-around space-y-8 mt-16 h-60">
+      <article class="flex flex-col flex-wrap justify-around space-y-6 mt-8 h-60">
         <Title class="w-lg" title="Title (max. 38 characters)" />
         <Banner class="max-w-lg" />
       </article>
